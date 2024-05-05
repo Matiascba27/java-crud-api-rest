44f7fdfe245d3ffa7df9afa6b7280a1e5efb1c24
Matiascba27 <matiasnunez1969@gmail.com>
2024-05-05 15:01:35 -0300

    Se agregan comentarios en la clase GlobalExceptionHandler

--- src/main/java/com/apirest/apirest/Exception/GlobalExceptionHandler.java
+++ src/main/java/com/apirest/apirest/Exception/GlobalExceptionHandler.java
@@ -5,24 +5,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+// Indica que esta clase es un controlador de excepciones
+@ControllerAdvice 
 public class GlobalExceptionHandler {
+
+    // Indica que el metodo siguiente es una excepcion global 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGlobalException(Exception ex) {
         return new ResponseEntity<>("Ocurrio un error: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 
     }
 
+    // Indica que el metodo siguiente es una excepcion personalizada NullPointerException
     @ExceptionHandler(NullPointerException.class)
     public ResponseEntity<String> handleNullPointerException(NullPointerException ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocurrio una excepcion: " + ex.getMessage());
     }
 
+    // Indica que el metodo siguiente es una excepcion personalizada IllegalArgumentException
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Ocurrio una excepcion de argumento ilegal: " + ex.getMessage());
     }
 
+    // Indica que el metodo siguiente es una excepcion personalizada UnsupportedOperationException
     @ExceptionHandler(UnsupportedOperationException.class)
     public ResponseEntity<String> handleUnsupportedOperationException(UnsupportedOperationException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Ocurrio una excepcion de operacion no compatible: " + ex.getMessage());
